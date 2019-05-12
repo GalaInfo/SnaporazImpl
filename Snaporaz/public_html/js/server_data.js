ef1177be88e1838aa72c281be9eaabfe2e3e691f
st159109 <borello.nazareno@educ.di.unito.it>
2019-05-12 19:13:03 +0200

    Inizio connessione SOAP[NON FUNZIONANTE]

--- Snaporaz/public_html/js/server_data.js
+++ Snaporaz/public_html/js/server_data.js
@@ -1,1 +1,2 @@
 const BASE_URL = "http://localhost:42729/SnaporazSpring/";
+const SOAP_URL = "http://localhost:42729";
--- Snaporaz/public_html/js/templateBuilder.js
+++ Snaporaz/public_html/js/templateBuilder.js
@@ -352,4 +352,18 @@ function buildSelect(data, defaultValue) {
     return init;
 }
 
-
+function buildSoapRequest(id, userId, project, amount) {
+    const template = `
+                    <?xml version="1.0" encoding="UTF-8"?><S:Envelope xmlns:S="http://schemas.xmlsoap.org/soap/envelope/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+                        <SOAP-ENV:Header/>
+                        <S:Body xmlns:ns2="http://ejb/">
+                            <ns2:addPayment>
+                                <id>ciao</id>
+                                <userId>ciao</userId>
+                                <project>23</project>
+                                <amount>32.0</amount>
+                            </ns2:addPayment>
+                        </S:Body>
+                    </S:Envelope>`;
+    return template;
+}
